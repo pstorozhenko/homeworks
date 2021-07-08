876fcd7a5964bddd1fdc84a1788271553af771df
Pavel Storozhenko <pstorozhenko@ptsecurity.com>
2021-07-08 21:43:28 +0300

    correct lists api

--- data_structures/main.c
+++ data_structures/main.c
@@ -12,11 +12,21 @@ struct forward_list_t {
     void *data;
 };
 
+// Создать новый список (создаёт один элемент списка)
 forward_list_t *forward_list_create();
+// Удалить список (удаляет все элементы списка)
 void forward_list_destroy(forward_list_t *head);
-void forward_list_add(forward_list_t *item);
-void forward_list_insert(forward_list_t *item);
+// Получить размер списка
+unsigned long forward_list_size(forward_list_t *head);
+// Изменить размер списока (лишние элементы удаляются, нехватающие создаются)
+void forward_list_resize(forward_list_t *head, unsigned long size);
+// Добавить новый элемент в конец списка (создаёт новый элемент списка и помещает его в конец)
+forward_list_t *forward_list_append(forward_list_t *head);
+// Вставить новый элемент после указанного (создаёт новый элемент списка и помещает его после item)
+forward_list_t *forward_list_insert(forward_list_t *item);
+// Удалить элемент из списка
 void forward_list_remove(forward_list_t *item);
+// Получить указатель на следующий элемент списка
 forward_list_t *forward_list_next(forward_list_t *item);
 
 
@@ -29,19 +39,28 @@ struct list_t {
     void *data;
 };
 
+// Создать новый список (создаёт один элемент списка)
 list_t *list_create();
+// Удалить список (удаляет все элементы списка)
 void list_destroy(list_t *head);
-void list_add(list_t *item);
-void list_insert(list_t *item);
+// Получить размер списка
+unsigned long list_size(list_t *head);
+// Изменить размер списока (лишние элементы удаляются, нехватающие создаются)
+void list_resize(list_t *head, unsigned long size);
+// Добавить новый элемент в конец списка (создаёт новый элемент списка и помещает его в конец)
+list_t *list_append(list_t *head);
+// Вставить новый элемент после указанного (создаёт новый элемент списка и помещает его после item)
+list_t *list_insert(list_t *item);
+// Удалить элемент из списка
 void list_remove(list_t *item);
+// Получить указатель на следующий элемент списка
 list_t *list_next(list_t *item);
+// Получить указатель на предыдущий элемент списка
 list_t *list_prev(list_t *item);
-
 
 
 int main()
 {
-
 
     return 0;
 }
